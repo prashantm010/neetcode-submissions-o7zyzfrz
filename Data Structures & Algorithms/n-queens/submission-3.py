669274e9a4ed708922a0359ad45e31d481a47413
prashant maheshwari <31507578+prashantm010@users.noreply.github.com>
2026-05-16 00:42:36 +0530

    Add: n-queens - submission-3

---
+++ Data Structures & Algorithms/n-queens/submission-3.py
@@ -0,0 +1,41 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        res = []
+        board = [["."] * n for _ in range(n)]
+
+        def backtrack(r):
+            if r == n:
+                copy = ["".join(row) for row in board]
+                res.append(copy)
+                return
+            for c in range(n):
+                if self.isSafe(r, c, board):
+                    board[r][c] = "Q"
+                    backtrack(r + 1)
+                    board[r][c] = "."
+
+        backtrack(0)
+        return res
+
+    def isSafe(self, row, col, board):
+        r = row - 1
+        while r >= 0:
+            if board[r][col] == "Q":
+                return False
+            r -= 1
+
+        r, c = row - 1, col - 1
+        while r >= 0 and c >= 0:
+            if board[r][c] == "Q":
+                return False
+            r = r - 1
+            c = c - 1
+
+        r, c = row - 1, col + 1
+        while r >= 0 and c <= len(board) - 1:
+            if board[r][c] == "Q":
+                return False
+            r = r - 1
+            c = c + 1
+
+        return True
